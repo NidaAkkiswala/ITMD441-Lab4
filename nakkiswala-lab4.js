94cfffd00da18d100c2897d555dfe229a4dd2dc0
Nida Akkiswala <nakkiswala@hawk.iit.edu>
2025-04-02 17:40:52 -0500

    hero heading

--- nakkiswala-lab4.js
+++ nakkiswala-lab4.js
@@ -1,3 +1,12 @@
 // Nida Akkiswala
 // ITMD 441 - Undergraduate Student
 
+(function () {
+    // Change Hero Main Headline
+    const heroHeading = document.querySelector(".hero h1");
+    if (heroHeading) {
+        heroHeading.textContent = "Uplift Your Brand with Stellar Marketing";
+    }
+}
+
+)
